aa37a745b8362d76466b1ea8c7c89ddc98821431
Spriggan matron Lindgren <slindgre@ga-h1.21-school.ru>
2019-10-29 21:44:06 +0300

    got test folder from develop branch

---
+++ test/test_validation.c
@@ -0,0 +1,95 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_validation.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gloras-t <gloras-t@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/27 16:26:01 by gloras-t          #+#    #+#             */
+/*   Updated: 2019/10/29 21:31:28 by gloras-t         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/corewar.h"
+
+enum octet
+{
+	FIRST = 0,
+	SECOND = 8,
+	THIRD = 16,
+	FORTH = 24
+};
+
+char	convert_ui_to_char(UI number, UI octet)
+{
+	char	c;
+	size_t	size;
+	UI		tmp;
+	UI		offset;
+
+	c = 0;
+	tmp = 0;
+	offset = octet;
+	size = 8 * sizeof(char);
+	while (size--)
+	{
+		tmp = number & (1 << octet);
+		tmp >>= offset;
+		c |= tmp;
+		octet++;
+	}
+	return c;
+}
+
+int main()
+{
+	char	buf[4];
+	buf[0] = 0x00;	buf[1] = 0xEA;	buf[2] = 0x83;	buf[3] = 0xF3;
+	assert(has_header(buf));
+
+	ft_bzero(buf, 4);
+	assert(!has_header(buf));
+
+	buf[0] = 0xF3; buf[1] = 0x83; buf[2] = 0xEA; buf[3] = 0x00;
+	assert(!has_header(buf));
+
+	buf[0] = 0x00; buf[1] = 0x83; buf[2] = 0xEA; buf[3] = 0xF3;
+	assert(!has_header(buf));
+
+	buf[0] = 0x00; buf[1] = 0xEA; buf[2] = 0x00; buf[3] = 0x00;
+	assert(!has_header(buf));
+
+	buf[0] = 0x00 + 1; buf[1] = 0xEA; buf[2] = 0x83; buf[3] = 0xF3;
+	assert(!has_header(buf));
+
+	buf[0] = 1; buf[1] = 1; buf[2] = 1; buf[3] = 1;
+	assert(!has_header(buf));
+
+	ft_printf("{green}OK{eoc} {yellow}has_header(char *byte);{eoc}\n");
+
+	assert(is_cor_extension("42.cor"));
+	assert(is_cor_extension("a.cor"));
+	assert(!is_cor_extension(""));
+	assert(is_cor_extension(".cor"));
+	assert(is_cor_extension("file.cor"));
+	assert(!is_cor_extension(".co"));
+	assert(!is_cor_extension(".c"));
+	assert(!is_cor_extension("a.cor "));
+	assert(!is_cor_extension("a.core"));
+	assert(!is_cor_extension("cor."));
+	assert(!is_cor_extension("roc."));
+	assert(is_cor_extension(" .cor"));
+	ft_printf("{green}OK{eoc} {yellow}is_cor_extension(char *file_name);{eoc}\n");
+/*
+	print_bits_ui(COREWAR_EXEC_MAGIC);
+	ft_printf("\n");
+	print_bits_char(convert_ui_to_char(COREWAR_EXEC_MAGIC, FIRST));
+	ft_printf("\n");
+	print_bits_char(convert_ui_to_char(COREWAR_EXEC_MAGIC, SECOND));
+	ft_printf("\n");
+	print_bits_char(convert_ui_to_char(COREWAR_EXEC_MAGIC, THIRD));
+	ft_printf("\n");
+	print_bits_char(convert_ui_to_char(COREWAR_EXEC_MAGIC, FORTH));
+*/
+    return (0);
+}
