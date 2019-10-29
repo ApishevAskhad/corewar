d7027a56906ede0c5e583a717d387b2a9fd72941
Spriggan matron Lindgren <slindgre@ga-h1.21-school.ru>
2019-10-29 21:46:07 +0300

    generic _free function set pointer to NULL after free

--- src/clean_utils.c
+++ src/clean_utils.c
@@ -6,7 +6,7 @@
 /*   By: slindgre <slindgre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/29 21:15:22 by slindgre          #+#    #+#             */
-/*   Updated: 2019/10/29 21:18:34 by slindgre         ###   ########.fr       */
+/*   Updated: 2019/10/29 21:44:52 by slindgre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,4 @@ void    _free(void *ptr)
 {
     free(ptr);
     ptr = NULL;   
-}
-
-void	error_exit(char *error, t_player *layer)
-{
-	if (error)
-		ft_putendl_fd(error, 1);
-	exit(1);
 }
