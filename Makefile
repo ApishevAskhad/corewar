28fb6243cda3445c27fe38d7f25fe79ed11709a4
Spriggan matron Lindgren <slindgre@ga-h1.21-school.ru>
2019-10-29 16:23:26 +0300

    op.h separated

--- Makefile
+++ Makefile
@@ -3,28 +3,28 @@
 #                                                         :::      ::::::::    #
 #    Makefile                                           :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: gloras-t <gloras-t@student.42.fr>          +#+  +:+       +#+         #
+#    By: slindgre <slindgre@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/10/07 20:31:24 by gloras-t          #+#    #+#              #
-#    Updated: 2019/10/28 21:23:28 by gloras-t         ###   ########.fr        #
+#    Updated: 2019/10/29 16:22:28 by slindgre         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-COREWAR = corewar
-ASM = asm
-TEST_VM = t
-OBJ_VM =	print_utils.o \
-			check_utils_01.o
-LIBFT = includes/ft_printf
-LIBFTP = $(LIBFT)/libftp.a
-HEADER = includes/corewar.h
-TEST_HEADER = includes/test_corewar.h
-FLAGS = -Wall -Wextra -Werror
+COREWAR 	= corewar
+ASM 		= asm
+TEST_VM 	= t
+OBJ_VM 		=	print_utils.o \
+				check_utils_01.o
+LIBFT 		= includes/ft_printf
+LIBFTP 		= $(LIBFT)/libftp.a
+HEADER 		= includes/corewar.h
+TEST_HEADER	= includes/test_corewar.h
+FLAGS 		= -Wall -Wextra -Werror
 
-ITALIC = \033[3m
-GREEN = \033[0;32m
-YELLOW = \033[1;33m
-EOC = \033[0m
+ITALIC 		= \033[3m
+GREEN 		= \033[0;32m
+YELLOW 		= \033[1;33m
+EOC 		= \033[0m
 
 all: $(COREWAR)
 $(COREWAR): $(LIBFTP) corewar.o $(OBJ_VM) $(HEADER)
--- includes/corewar.h
+++ includes/corewar.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   corewar.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gloras-t <gloras-t@student.42.fr>          +#+  +:+       +#+        */
+/*   By: slindgre <slindgre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/26 21:44:41 by gloras-t          #+#    #+#             */
-/*   Updated: 2019/10/28 23:32:20 by gloras-t         ###   ########.fr       */
+/*   Updated: 2019/10/29 16:18:55 by slindgre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,58 +14,11 @@
 # define COREWAR_H
 
 # include "libft/libft.h"
+# include "op.h"
 # include <assert.h>
-
-# define IND_SIZE			2
-# define REG_SIZE			4
-# define DIR_SIZE			REG_SIZE
-
-# define REG_CODE			1
-# define DIR_CODE			2
-# define IND_CODE			3
-
-# define MAX_ARGS_NUMBER	4
-# define MAX_PLAYERS		4
-# define MEM_SIZE			(4 * 1024)
-# define IDX_MOD			(MEM_SIZE / 8)
-# define CHAMP_MAX_SIZE		(MEM_SIZE / 6)
-
-# define COMMENT_CHAR		'#'
-# define LABEL_CHAR			':'
-# define DIRECT_CHAR		'%'
-# define SEPARATOR_CHAR		','
-
-# define LABEL_CHARS		"abcdefghijklmnopqrstuvwxyz_0123456789"
-
-# define NAME_CMD_STRING	".name"
-# define COMMENT_CMD_STRING	".comment"
-
-# define REG_NUMBER			16
-
-# define CYCLE_TO_DIE		1536
-# define CYCLE_DELTA		50
-# define NBR_LIVE			21
-# define MAX_CHECKS			10
-
-# define T_REG				1
-# define T_DIR				2
-# define T_IND				4
-# define T_LAB				8
-
-# define PROG_NAME_LENGTH	128
-# define COMMENT_LENGTH		2048
-# define COREWAR_EXEC_MAGIC	0xEA83F3
 
 # define COR_EXTENSION		".cor"
 # define UI					unsigned int
-
-typedef struct				s_cor_file
-{
-	UI						magic;
-	char					name[PROG_NAME_LENGTH + 1];
-	UI						code_size;
-	char					comment[COMMENT_LENGTH + 1];
-}							t_cor_file;
 
 int							ft_printf(const char *restrict format, ...);
 
