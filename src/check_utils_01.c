2a5a14174a857a57963ca4e67bb0bc0b8455381d
ApishevAskhad <aaa_mt@bk.ru>
2019-11-03 19:35:56 +0300

    - refactor has_header(); (#4)
    - add trash.c for removed functions;

--- src/check_utils_01.c
+++ src/check_utils_01.c
@@ -6,7 +6,7 @@
 /*   By: gloras-t <gloras-t@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/28 21:20:58 by gloras-t          #+#    #+#             */
-/*   Updated: 2019/11/02 23:42:13 by gloras-t         ###   ########.fr       */
+/*   Updated: 2019/10/29 22:56:01 by gloras-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,38 +26,16 @@ int		is_cor_extension(char *file_name)
 	return (0);
 }
 
-int		check_octet(char c, UI octet)
-{
-	UI		a;
-	UI		b;
-	UI		i;
-	size_t	char_size;
-	size_t	ui_size;
-
-	i = 0;
-	char_size = 8 * sizeof(char);
-	ui_size = 8 * sizeof(UI);
-	while (char_size--)
-	{
-		a = c & (1 << i);
-		b = COREWAR_EXEC_MAGIC & (1 << (i + ui_size - octet));
-		if ((a > 0 && b == 0) || (a == 0 && b > 0))
-			return (0);
-		i++;
-	}
-	return (1);
-}
-
 /*
 **	not NULL safety
 */
 int		has_header(char *byte)
 {
 	assert(byte != NULL);
-	if (check_octet(byte[0], 8)
-	&& check_octet(byte[1], 16)
-	&& check_octet(byte[2], 24)
-	&& check_octet(byte[3], 32))
+	if (((COREWAR_EXEC_MAGIC & 0xFF000000) >> 24) == ((UI)byte[0] & 0xFF)
+	&& ((COREWAR_EXEC_MAGIC & 0xFF0000) >> 16) == ((UI)byte[1] & 0xFF)
+	&& ((COREWAR_EXEC_MAGIC & 0xFF00) >> 8) == ((UI)byte[2] & 0xFF)
+	&& (COREWAR_EXEC_MAGIC & 0xFF) == ((UI)byte[3] & 0xFF))
 		return (1);
 	return (0);
 }
--- test/test_validation.c
+++ test/test_validation.c
@@ -6,40 +6,11 @@
 /*   By: gloras-t <gloras-t@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/27 16:26:01 by gloras-t          #+#    #+#             */
-/*   Updated: 2019/11/03 00:50:59 by gloras-t         ###   ########.fr       */
+/*   Updated: 2019/10/29 22:56:23 by gloras-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
-
-enum octet
-{
-	FIRST = 0,
-	SECOND = 8,
-	THIRD = 16,
-	FORTH = 24
-};
-
-char	convert_ui_to_char(UI number, UI octet)
-{
-	char	c;
-	size_t	size;
-	UI		tmp;
-	UI		offset;
-
-	c = 0;
-	tmp = 0;
-	offset = octet;
-	size = 8 * sizeof(char);
-	while (size--)
-	{
-		tmp = number & (1 << octet);
-		tmp >>= offset;
-		c |= tmp;
-		octet++;
-	}
-	return c;
-}
 
 int main()
 {
