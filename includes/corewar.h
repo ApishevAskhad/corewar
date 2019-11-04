48589fe3cd145978e56977016817f7fac8bfcffd
Spriggan matron Lindgren <slindgre@ga-l3.21-school.ru>
2019-11-04 19:47:30 +0300

    check_players_nbrs()

--- includes/corewar.h
+++ includes/corewar.h
@@ -6,7 +6,7 @@
 /*   By: slindgre <slindgre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/26 21:44:41 by gloras-t          #+#    #+#             */
-/*   Updated: 2019/10/29 20:23:55 by slindgre         ###   ########.fr       */
+/*   Updated: 2019/11/04 19:38:46 by slindgre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,26 @@
 
 # define COR_EXTENSION		".cor"
 # define UI					uint32_t
+# define UC					unsigned char
+# define DEBUG				1
 
 typedef struct				s_player
 {
-	UI      			    magic;
+	UI						magic;
+	UI						prog_size;
+	UC						code[CHAMP_MAX_SIZE];
 	char					prog_name[PROG_NAME_LENGTH + 1];
-	UI      			    prog_size;
 	char					comment[COMMENT_LENGTH + 1];
-	unsigned char			code[CHAMP_MAX_SIZE + 1];
 }							t_player;
 
+typedef struct				s_game
+{
+	t_player				players[MAX_PLAYERS];
+	int						players_nbr;
+	UC						mem[MEM_SIZE];
+	int						dump;
+	int						visual;
+}							t_game;
 
 int							ft_printf(const char *restrict format, ...);
 
@@ -36,9 +46,10 @@ int							ft_printf(const char *restrict format, ...);
 ** print_utils.c
 */
 void						print_usage(void);
-void						print_error(char *error_msg);
-void						print_bits_ui(UI     number);
+void						print_error(char *error_msg, char *name);
+void						print_bits_ui(UI number);
 void						print_bits_char(char number);
+void						print_dump(UC *ptr, size_t size);
 
 /*
 ** check_utils_01.c
@@ -46,5 +57,24 @@ void						print_bits_char(char number);
 int							is_cor_extension(char *file_name);
 int							has_header(char *byte);
 
+/*
+** clean_utils.c
+*/
+void						_free(void *ptr);
 
+/*
+** create_player.c
+*/
+t_player					create_player(int fd, char *file_name);
+int							check_file(char *file_name);
+
+/*
+** init_game.c
+*/
+void						init_game(t_game *game);
+
+/*
+** place_players.c
+*/
+void    					place_players_code(t_game *game);
 #endif
