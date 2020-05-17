eff136dbfc942f70c84312ef6f815a4bfc976577
akaruitori <dsabh@ya.ru>
2020-05-17 23:12:00 +0300

    Add binary reading and saving As well as some asm-reading refactoring

--- src_asm/file_reading.c
+++ src_asm/file_reading.c
@@ -6,7 +6,7 @@
 /*   By: dtimeon <dtimeon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/11 01:45:26 by dtimeon           #+#    #+#             */
-/*   Updated: 2020/05/16 05:20:50 by dtimeon          ###   ########.fr       */
+/*   Updated: 2020/05/17 23:05:11 by dtimeon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,14 +44,40 @@ static void				read_asm_file(t_file *file)
 		if (is_not_blank_str(buffer) && is_not_comment(buffer))
 			add_new_line(file);
 	}
-	if (line_num > 2 && read_status == 0)
+	if ((file->first_line != file->last_line) && line_num > 2 &&
+		read_status == 0)
 		file->is_read_successfully = 1;
+	else if (file->first_line == file->last_line)
+		print_error(file->filename, "Incorrect file, at least "
+									"champion name and comment are required");
+	else
+		print_error(file->filename, "File reading error");
 }
 
 static void					read_binary_file(t_file *file)
 {
-	// TODO: reads and saves binary code segment by segment
-	(void)file;
+	ssize_t				min_size;
+	unsigned char		*buffer;
+	ssize_t				bytes_read;
+
+	min_size = 2 * sizeof(unsigned int) + PROG_NAME_LENGTH + COMMENT_LENGTH + 8;
+	buffer = (unsigned char *)malloc(sizeof(unsigned char) * min_size);
+	if (!buffer)
+		exit_with_allocation_error(file->filename);
+	bytes_read = read(file->fd, buffer, min_size);
+	if (bytes_read < min_size)
+	{
+		ft_memdel((void **)&buffer);
+		if (bytes_read < 0)
+			print_error(file->filename, "File reading error");
+		else if (bytes_read < min_size)
+			print_error(file->filename, "Binary file is too small");
+	}
+	else
+	{
+		file->binary_header = buffer;
+		read_binary_champ_code(file, min_size);			
+	}
 }
 
 t_file					*read_file(int fd, char *filename)
